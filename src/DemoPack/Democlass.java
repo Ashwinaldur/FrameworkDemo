d6dfcc87e7d4ba006a145a997d16ff684d584277
Ashwinaldur <36203968+Ashwinaldur@users.noreply.github.com>
2018-02-08 18:31:22 +0530

    Update Democlass.java

--- src/DemoPack/Democlass.java
+++ src/DemoPack/Democlass.java
@@ -3,5 +3,6 @@ package DemoPack;
 public class Democlass {
 public static void main(String[] args){
 	System.out.println("welcome to cognizant");
+	System.out.println("Ashwin is working in cts");
 }
 }
